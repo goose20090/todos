22880626ebab57587498626fdd6f7aed4caee917
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-08-27 13:30:06 +0100

    extracted session[:user_email] to application controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,7 @@
 class TodosController < ApplicationController
     before_action :authenticate
     def index
-        @todos = Todo.where(email: session[:current_email])
+        @todos = Todo.where(email: current_email)
     end
 
     def new
@@ -10,7 +10,7 @@ class TodosController < ApplicationController
 
     def create
         redirect_to todos_path
-        todo = Todo.create(todo_params.merge(email: session[:current_email]))
+        todo = Todo.create(todo_params.merge(email: current_email))
     end
 
     private
